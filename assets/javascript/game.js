e31135711c130ede8b4a6d744b10af0c7750def2
afletch24 <anfletcher24@gmail.com>
2017-08-10 17:37:41 -0500

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,24 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var wrongGuesses = [];
 
 var partialWord = "------".split("");
+console.log(partialWord);
+
+
 var lives ;
+
+// Get Elements
+var showLives = document.getElementById("myLives");
+
+// User presses key to start.
+function startgame () {
+    remainingGuesses: 15;
+    
+    
+}
+
+// // blanks
+// correctGuesses.innerHTML = partialWord();
+// var correctGuesses = document.getElementById(" ");
 
 
 // Generate Random Word 
@@ -15,38 +32,65 @@ function randomWord(){
     return random;
 };
 
+
 var word = randomWord();
 console.log(word);
 
 
 
 
-
-
-
+// start
 document.onkeyup = function(event){
     var userGuesses = event.key;
     var userGuess = userGuesses.toLowerCase();
     console.log(userGuess);
+  
 
-    // if user does not type use a letter
+    // if user does not type a letter
     if(alphabet.indexOf(userGuess) == -1){
         console.log("you're an idiot...");
     }
-     
-    for(var i=0; i < word.length; i++){
-        if(word[i] == userGuess ){
-            partialWord[i] = word[i];
-        }
+    
+    if(word.indexOf(userGuess) === -1){
+
+        wrongGuesses.push(userGuess);
+        document.getElementById("box").innerHTML = wrongGuesses;
+        
+        document.getElementById("guessesRemaining").innerHTML =remainingGuesses;
+        remainingGuesses --;
+
+       if(remainingGuesses == -1){
+           document.getElementById("guessesRemaining").innerHTML =("Game over");
+       }
+
     }
 
-    console.log(partialWord.join(" "));
-        if(partialWord.indexOf('-') == -1){
-            console.log("YOU WIN!");
+    else
+    {
+        
+        // if the user guess if part of the word...
+        for(var i=0; i < word.length; i++){
+            if(word[i] == userGuess ){
+            partialWord[i] = word[i];
+            }
         }
+
+        word.innerHTML=partialWord.join(" ");
+        console.log(partialWord.join (" "));
+        document.getElementById("currentGuesses").innerHTML =partialWord;
+    }
+   
+    if(partialWord.indexOf('-') == -1){
+        console.log("you win!");
+        wins++;
+        alert("You Win!");
+
+    }
     
-       
-    };
+
+
 
     
-
+    
+};
+    
